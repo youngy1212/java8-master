4315c1d0c2d8c6b2cf8045f958381d3196d1177c
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2023-12-23 18:26:29 +0900

    초기 코

---
+++ src/main/java/me/whiteship/java8to11/RunSometing.java
@@ -0,0 +1,12 @@
+package me.whiteship.java8to11;
+
+public interface RunSometing {
+
+    void doIt(); //단 하나의 추상메소드 가 있으면 함수형 인터페이스
+    //앞에 abstract(추상) 생략되어있음 
+
+    static void printName(){
+
+    }
+
+}
